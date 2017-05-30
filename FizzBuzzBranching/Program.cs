62ad3bf7cd5e2a7e05fd822d611ede46428fe8ed
NoahSekreter <nsekreter@gmail.com>
2017-05-30 11:23:07 -0400

    Init commit

---
+++ FizzBuzzBranching/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzBranching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    result += "Fizz";
+                }
+                if(i % 5 == 0)
+                {
+                    result += "Buzz";
+                }
+                Console.WriteLine(i + " : " + result);
+                result = "";
+            }
+        }
+    }
+}
